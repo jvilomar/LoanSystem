7f0fd6caf4caf7963598681fda7761278fe8300f
Jose Manuel Vilomar <jvilomar@gmail.com>
2021-01-04 11:21:57 -0400

    Created Repository Structure and added base repository type.

---
+++ LoanSystemApp/Common.Repository/Interfaces/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Repository.Interfaces
+{
+    public interface IRepository<TEntity> where TEntity : class, new()
+    {
+        TEntity Get(int id);
+        IEnumerable<TEntity> GetAll();
+        IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> predicate);
+
+        Task<TEntity> AddAsync(TEntity entity);
+        Task<TEntity> AddRangeAsync(IEnumerable<TEntity> entities);
+        Task<TEntity> RemoveAsync(TEntity entity);
+    }
+}
---
+++ LoanSystemApp/Common.Repository/Repositories/Repository.cs
@@ -0,0 +1,65 @@
+﻿using Common.Repository.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Repository.Repositories
+{
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : class, new()
+    {
+        protected readonly DbContext Context;
+        public Repository(DbContext context)
+        {
+            Context = context;
+        }
+
+        public async Task<TEntity> AddAsync(TEntity entity)
+        {
+            try
+            {
+                await Context.Set<TEntity>().AddAsync(entity);
+                return entity;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"{nameof(entity)} could not be saved: {ex.Message}");
+            }
+        }
+
+        public async void AddRangeAsync(IEnumerable<TEntity> entities)
+        {
+            try
+            {
+                await Context.Set<TEntity>().AddRangeAsync(entities);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"adding multiple entities could not be completed: {ex.Message}");
+            }
+        }
+
+        public IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> predicate)
+        {
+            return Context.Set<TEntity>().Where(predicate);
+        }
+
+        public TEntity Get(int id)
+        {
+            return Context.Set<TEntity>().Find(id);
+        }
+
+        public IEnumerable<TEntity> GetAll()
+        {
+            return Context.Set<TEntity>().ToList();
+        }
+
+        public Task<TEntity> RemoveAsync(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
